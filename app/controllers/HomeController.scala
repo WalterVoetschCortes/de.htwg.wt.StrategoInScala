449ea88b654f57304f3e5a991fbef8d7aa5accd3
WalterVoetschCortes <walter.vxd@googlemail.com>
2020-10-26 11:09:13 +0100

    Put some GETs in routes and call tui in HomeController.scala. Handle must be fixed

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,12 +13,40 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   val gameController = Stratego.controller
   val matchfieldString = gameController.matchFieldToString
 
-  def about() = Action {
+  def index() = Action {
     Ok(views.html.index())
   }
 
   def stratego = Action {
     Ok(matchfieldString)
+  }
+
+  def newGame() = Action {
+    Ok(Stratego.tui.processInputLine("n"))
+  }
+
+  def load() = Action {
+    Ok(Stratego.tui.processInputLine("l"))
+  }
+
+  def save() = Action {
+    Ok(Stratego.tui.processInputLine("s"))
+  }
+
+  def undo() = Action {
+    Ok(Stratego.tui.processInputLine("z"))
+  }
+
+  def redo() = Action {
+    Ok(Stratego.tui.processInputLine("r"))
+  }
+
+  def quit() = Action {
+    Ok(Stratego.tui.processInputLine("q"))
+  }
+
+  def handle(input: String) = Action {
+    Ok(Stratego.tui.processInputLine(input))
   }
 
 }
